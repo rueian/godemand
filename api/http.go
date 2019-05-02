83368ab5c5845fd7f6029dcfef80846169565218
Rueian <rueiancsie@gmail.com>
2019-05-03 00:24:28 +0800

    feat: http transport impl

---
+++ api/http.go
@@ -0,0 +1,99 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/rueian/godemand/dao"
+	"github.com/rueian/godemand/types"
+	"golang.org/x/xerrors"
+)
+
+func NewHTTPMux(s types.Service) *http.ServeMux {
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/RequestResource", func(writer http.ResponseWriter, request *http.Request) {
+		request.ParseForm()
+
+		poolID := request.Form.Get("poolID")
+
+		client, ok := handleClient(writer, request.Form.Get("client"))
+		if !ok {
+			return
+		}
+
+		res, err := s.RequestResource(poolID, client)
+		if handleErr(writer, err) {
+			return
+		}
+
+		writeRes(writer, res)
+	})
+	mux.HandleFunc("/GetResource", func(writer http.ResponseWriter, request *http.Request) {
+		request.ParseForm()
+
+		poolID := request.Form.Get("poolID")
+		id := request.Form.Get("id")
+
+		res, err := s.GetResource(poolID, id)
+		if handleErr(writer, err) {
+			return
+		}
+
+		writeRes(writer, res)
+	})
+	mux.HandleFunc("/Heartbeat", func(writer http.ResponseWriter, request *http.Request) {
+		request.ParseForm()
+
+		poolID := request.Form.Get("poolID")
+		id := request.Form.Get("id")
+		client, ok := handleClient(writer, request.Form.Get("client"))
+		if !ok {
+			return
+		}
+
+		err := s.Heartbeat(poolID, id, client)
+		if handleErr(writer, err) {
+			return
+		}
+
+		writer.WriteHeader(200)
+	})
+	return mux
+}
+
+func writeRes(w http.ResponseWriter, resource types.Resource) {
+	ba, err := json.Marshal(resource)
+	if err != nil {
+		handleErr(w, err)
+	} else {
+		w.WriteHeader(200)
+		w.Write(ba)
+	}
+}
+
+func handleClient(w http.ResponseWriter, input string) (client types.Client, ok bool) {
+	err := json.Unmarshal([]byte(input), &client)
+	if err != nil {
+		w.WriteHeader(422)
+		w.Write([]byte("fail to parse the the client field"))
+		return types.Client{}, false
+	}
+	return client, true
+}
+
+func handleErr(w http.ResponseWriter, err error) bool {
+	if xerrors.Is(err, dao.AcquireLaterErr) {
+		w.WriteHeader(429)
+	} else if xerrors.Is(err, dao.PoolNotFoundErr) {
+		w.WriteHeader(404)
+	} else if xerrors.Is(err, ResourceNotFoundErr) {
+		w.WriteHeader(404)
+	} else if err != nil {
+		w.WriteHeader(500)
+	}
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return true
+	}
+	return false
+}
---
+++ api/http_test.go
@@ -0,0 +1,211 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/rueian/godemand/config"
+	"github.com/rueian/godemand/dao"
+	"github.com/rueian/godemand/plugin"
+	"github.com/rueian/godemand/types"
+	"github.com/rueian/godemand/types/mock"
+)
+
+var _ = Describe("NewHTTPMux", func() {
+	var ctrl *gomock.Controller
+	var service *mock.MockService
+	var mux *http.ServeMux
+	var req *http.Request
+	var rec *httptest.ResponseRecorder
+	var endpoint string
+	var form url.Values
+	var poolID string
+	var resID string
+	var client types.Client
+	var clientJson string
+	var res types.Resource
+	var err error
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		service = mock.NewMockService(ctrl)
+		mux = NewHTTPMux(service)
+		rec = httptest.NewRecorder()
+		form = url.Values{}
+		poolID = "pool1"
+		resID = "res1"
+		client = types.Client{ID: "client1", Meta: map[string]interface{}{"ip": "0.0.0.0"}}
+		bc, _ := json.Marshal(client)
+		clientJson = string(bc)
+		res = types.Resource{}
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	JustBeforeEach(func() {
+		req = httptest.NewRequest("POST", endpoint, strings.NewReader(form.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		mux.ServeHTTP(rec, req)
+		if rec.Body.Len() > 0 {
+			err = json.Unmarshal(rec.Body.Bytes(), &res)
+		}
+	})
+
+	Describe("/RequestResource", func() {
+		BeforeEach(func() {
+			endpoint = "/RequestResource"
+		})
+		Context("no client", func() {
+			BeforeEach(func() {
+				form.Add("poolID", poolID)
+			})
+			It("err", func() {
+				Expect(rec.Code).To(Equal(422))
+			})
+		})
+		Context("param correct", func() {
+			BeforeEach(func() {
+				form.Add("poolID", poolID)
+				form.Add("client", clientJson)
+			})
+
+			for _, c := range []errorCase{
+				makeErrorCase("no pool", 404, types.Resource{}, dao.PoolNotFoundErr),
+				makeErrorCase("lock fail", 429, types.Resource{}, dao.AcquireLaterErr),
+				makeErrorCase("no config", 500, types.Resource{}, config.PoolConfigNotFoundErr),
+				makeErrorCase("no plugin", 500, types.Resource{}, plugin.ControllerNotFoundErr),
+				makeErrorCase("other err", 500, types.Resource{}, errors.New("random")),
+			} {
+				func(c errorCase) {
+					Context(c.Name, func() {
+						BeforeEach(func() {
+							service.EXPECT().RequestResource(poolID, client).Return(c.Returns...)
+						})
+						It("err", func() {
+							Expect(rec.Code).To(Equal(c.ExpectCode))
+						})
+					})
+				}(c)
+			}
+
+			Context("success", func() {
+				BeforeEach(func() {
+					service.EXPECT().RequestResource(poolID, client).Return(types.Resource{ID: resID}, nil)
+				})
+				It("got res", func() {
+					Expect(rec.Code).To(Equal(200))
+					Expect(err).NotTo(HaveOccurred())
+					Expect(res).To(Equal(types.Resource{ID: resID}))
+				})
+			})
+		})
+	})
+
+	Describe("/GetResource", func() {
+		BeforeEach(func() {
+			endpoint = "/GetResource"
+		})
+		Context("param correct", func() {
+			BeforeEach(func() {
+				form.Add("poolID", poolID)
+				form.Add("id", resID)
+			})
+
+			for _, c := range []errorCase{
+				makeErrorCase("no pool", 404, types.Resource{}, dao.PoolNotFoundErr),
+				makeErrorCase("no res", 404, types.Resource{}, ResourceNotFoundErr),
+				makeErrorCase("other err", 500, types.Resource{}, errors.New("random")),
+			} {
+				func(c errorCase) {
+					Context(c.Name, func() {
+						BeforeEach(func() {
+							service.EXPECT().GetResource(poolID, resID).Return(c.Returns...)
+						})
+						It("err", func() {
+							Expect(rec.Code).To(Equal(c.ExpectCode))
+						})
+					})
+				}(c)
+			}
+
+			Context("success", func() {
+				BeforeEach(func() {
+					service.EXPECT().GetResource(poolID, resID).Return(types.Resource{ID: resID}, nil)
+				})
+				It("got res", func() {
+					Expect(rec.Code).To(Equal(200))
+					Expect(err).NotTo(HaveOccurred())
+					Expect(res).To(Equal(types.Resource{ID: resID}))
+				})
+			})
+		})
+	})
+
+	Describe("/Heartbeat", func() {
+		BeforeEach(func() {
+			endpoint = "/Heartbeat"
+		})
+		Context("no client", func() {
+			BeforeEach(func() {
+				form.Add("poolID", poolID)
+				form.Add("id", resID)
+			})
+			It("err", func() {
+				Expect(rec.Code).To(Equal(422))
+			})
+		})
+		Context("param correct", func() {
+			BeforeEach(func() {
+				form.Add("poolID", poolID)
+				form.Add("id", resID)
+				form.Add("client", clientJson)
+			})
+
+			for _, c := range []errorCase{
+				makeErrorCase("no pool", 404, dao.PoolNotFoundErr),
+				makeErrorCase("no res", 404, ResourceNotFoundErr),
+				makeErrorCase("lock fail", 429, dao.AcquireLaterErr),
+				makeErrorCase("other err", 500, errors.New("random")),
+			} {
+				func(c errorCase) {
+					Context(c.Name, func() {
+						BeforeEach(func() {
+							service.EXPECT().Heartbeat(poolID, resID, client).Return(c.Returns...)
+						})
+						It("err", func() {
+							Expect(rec.Code).To(Equal(c.ExpectCode))
+						})
+					})
+				}(c)
+			}
+
+			Context("success", func() {
+				BeforeEach(func() {
+					service.EXPECT().Heartbeat(poolID, resID, client).Return(nil)
+				})
+				It("got res", func() {
+					Expect(rec.Code).To(Equal(200))
+				})
+			})
+		})
+	})
+})
+
+type errorCase struct {
+	Name       string
+	Returns    []interface{}
+	ExpectCode int
+}
+
+func makeErrorCase(name string, expect int, returns ...interface{}) errorCase {
+	return errorCase{name, returns, expect}
+}
